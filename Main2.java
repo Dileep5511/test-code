29050e91e853bcff0b314e281195118a929b863e
DILEEP KUMAR SADI <60293365+Dileep5511@users.noreply.github.com>
2022-11-21 15:52:18 +0530

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main
+{
+    public static int countLastChar(String str1,String str2){
+        int count=0;
+        char ch=str2.charAt(str2.length()-1);
+        int i=0;
+        while(i<str1.length()){
+            if(str1.charAt(i)==ch){
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String str1=sc.nextLine();
+	    String str2=sc.nextLine();
+		System.out.println(countLastChar(str1,str2));
+	}
+}
